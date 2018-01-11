1581fad8533b28a75046c6ae8664b5a63a1412cd
Aveheuzed <a.masson555@ntymail.com>
2018-01-11 18:01:09 +0100

    First stable version !

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ from tkinter import tix
 import mtgsdk
 
 LANG = "French"
+CARD_BOTTOM = "http://upload.wikimedia.org/wikipedia/en/a/aa/Magic_the_gathering-card_back.jpg"
+CARD_DIM = (223,310)
+
+def list_sets():
+        sets = mtgsdk.Set.all()
+        vals = [(s.name, s.code) for s in sorted(sets, key=lambda x:x.release_date, reverse=True)
+        ]
+        return vals
+
 
 class Card(mtgsdk.Card) :
 
@@ -56,18 +65,21 @@ class Card(mtgsdk.Card) :
         def getimg(card):
                 """Retrives and returns the picture of the card
                 if possible, in the global LANG language"""
-                if card.foreign_names is None :
+                if card.image_url is None :
+                        url = CARD_BOTTOM
+                elif card.foreign_names is None :
                         url = card.image_url
                 else :
                         url = None
                         for l in card.foreign_names :
                                 if l["language"] == LANG :
-                                        url = l["imageUrl"]
+                                        url = l.get("imageUrl", CARD_BOTTOM)
                                         break
                         if url is None :
                                 url = card.image_url
                 data = io.BytesIO(urllib.request.urlopen(url).read())
                 img = PIL.Image.open(data)
+                img = img.resize(CARD_DIM)
                 return PIL.ImageTk.PhotoImage(img)
 
 
@@ -94,6 +106,10 @@ class CardPresenter :
                 # bottom : add-a-card Button
                 self.addacard = tix.Button(self.main, text=" + ", command=self.search)
 
+                # bottom : set codes index
+                self.askcode =  tix.Button(self.main, text=" ? ", command=self.showsets)
+                self.set_codes = None
+
                 self.main.bind_all("<Control-s>", self.save)
                 self.main.bind_all("<Control-o>", self.load)
                 self.names.listbox.bind_all("<<ListboxSelect>>", self.display_card)
@@ -104,8 +120,9 @@ class CardPresenter :
 
                 self.main.pack(fill="both",expand=True)
                 self.names.place(x=0, y=0, anchor="nw", relheight=1.0)
-                self.imglbl.place(anchor="ne", relx=1.0, rely=0.0, width=223, height=310)
+                self.imglbl.place(anchor="ne", relx=1.0, rely=0.0, width=CARD_DIM[0], height=CARD_DIM[1])
                 self.addacard.place(anchor="se", relx=1.0, rely=1.0)
+                self.askcode.place(anchor="se", relx=0.9, rely=1.0)
 
         def save(self, dummy_arg=None):
                 # We should also find a way to store the pictures
@@ -147,21 +164,23 @@ class CardPresenter :
                         self.imglbl.configure(image=self.curimg)
                         self.curindex = index
 
-        def search(self):
+        def search(self,initial=str()):
                 # 1. Ask the card ID (set + number)
-                resp = askstring("Ajouter une carte", "Saisissez l'identifiant à 6 caractères en bas à gauche de la carte")
+                resp = askstring("Ajouter une carte", "Saisissez l'identifiant à 6 caractères en bas à gauche de la carte", initialvalue=initial)
                 if resp is None :
                         return
-                if not re.match("[A-Z][A-Z0-9]{2}[0-9]{1,3}", resp) :
+                z = re.fullmatch("(?P<code>p?[A-Z][A-Z0-9]{2})(?P<number>[0-9]{1,3})", resp)
+                if not z :
                         showerror("Saisie incorrecte", "Assurez-vous d'avoir correctement saisi l'identifiant (en respectant la casse)")
-                set_id, num = resp[:3], int(resp[3:])
+                        return
+                set_id, num = z.groupdict()["code"], int(z.groupdict()["number"])
 
                 # 2. Proceed the request
                 rq = Card.where(language=LANG, set=set_id, number=num).all()
                 if not len(rq) :
                         showerror("Aucune carte trouvée", "Le service distant ne répond pas, ou vérifiez votre saisie")
                         return
-                elif len(rq) > 1 :
+                elif len(rq) > 1 : # never happens yet
                         tl = tix.TopLevel(self)
                         lf = tix.LabelFrame(tl, text="Carte(s) trouvée(s), cliquez sur la bonne")
                         lf.pack(fill="both",expand=True)
@@ -218,6 +237,23 @@ class CardPresenter :
                         return -1
                 else :
                         card -= 1
+
+        def showsets(self):
+                if self.set_codes is None :
+                        self.set_codes = list_sets()
+                tl = tix.Toplevel(self.main.master)
+                lb = tix.ScrolledListBox(tl)
+                lb.listbox.insert(0, *[x[0] for x in self.set_codes])
+                lb.listbox.configure(width=30, height=30, bg="#ffffff")
+                tl.bind("<Escape>", tl.destroy)
+                def finish(event):
+                        index = lb.listbox.curselection()
+                        if not len(index) :
+                                return
+                        self.search(self.set_codes[index[0]][1])
+                        tl.destroy()
+                lb.listbox.bind("<<ListboxSelect>>", finish)
+                lb.pack(fill="both",expand=True)
 
 
 if __name__ == '__main__':
